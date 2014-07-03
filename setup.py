64dd5bdb95d06571625b6317db957d7ba787d56e
Christoph Zwerschke <cito@online.de>
2014-07-03 23:46:56 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ docs_extras = ['Sphinx']
 
 setup(
     name='pyramid_ldap3',
-    version='0.2',
+    version='0.3',
     description='pyramid_ldap3',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
