5329330788d9ae81da44f765e2843e4a7d8f06bc
Christoph Zwerschke <cito@online.de>
2019-04-04 00:20:34 +0200

    Add new keyword arg to setup function
    Just for the sake of completeness and because twine check complains.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     version='0.4.1',
     description='pyramid_ldap3',
     long_description=README + '\n\n' + CHANGES,
+    long_description_content_type='text/x-rst',
     classifiers=[
         "Framework :: Pyramid",
         "Programming Language :: Python",
